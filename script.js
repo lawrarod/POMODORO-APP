cda94dc6e7214a44a0fed86d8d9ed33d064494c3
lawrarod <zayumlaura@gmail.com>
2023-08-14 23:27:53 +0200

    "timer intervals updated"

--- script.js
+++ script.js
@@ -4,31 +4,52 @@ const resetButton = document.getElementById("reset");
 const timer = document.getElementById("minutes");
 const time = document.getElementById("time");
 const saraLuna = new Audio("sara-luna.mp3");
+const workMode = document.getElementById("work");
+const breakMode = document.getElementById("break");
 
+const toggleClassWork = () => {
+  workMode.classList.toggle("active");
+};
 
-let interval, timeLeft = 5;
+const toggleClassBreak = () => {
+  breakMode.classList.toggle("active");
+};
 
+workMode.addEventListener("click", toggleClassWork);
+breakMode.addEventListener("click", toggleClassBreak);
 
+// global variables
+let interval,
+    timeLeft = 5,
+    breakTime = 5;
+
+// main timer functions
 const updateTimer = () => {
-  let minutes = Math.floor(timeLeft/60);
+  let minutes = Math.floor(timeLeft / 60);
   let seconds = timeLeft % 60;
-  let formattedTime = `${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`;
+  let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
 
   time.innerHTML = formattedTime;
 };
 
 const startTimer = () => {
-  interval = setInterval(()=>{
+  workMode.classList.add("active");
+  breakMode.classList.remove("active");
+  interval = setInterval(() => {
     timeLeft--;
     updateTimer();
-    if(timeLeft === 0){
+
+    if (timeLeft === 0) {
+      toggleClassWork(false);
       clearInterval(interval);
       saraLuna.play();
       timeLeft = 1500;
       updateTimer();
+      startBreak();
     }
-  },1000);
-
+  }, 1000);
 };
 
 const stopTimer = () => {
@@ -37,9 +58,33 @@ const stopTimer = () => {
 
 const resetTimer = () => {
   clearInterval(interval);
-  timeLeft = 1500;  
+  timeLeft = 1500;
   updateTimer();
+};
 
+// break timer
+const startBreak = () => {
+  interval = setInterval(() => {
+    breakMode.classList.add("active");
+    workMode.classList.remove("active");
+
+    let minutes = Math.floor(breakTime / 60);
+    let seconds = breakTime % 60;
+    let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+
+    time.innerHTML = formattedTime;
+
+    breakTime--;
+
+    if(breakTime === 0){
+      toggleClassBreak()
+      clearInterval(interval);
+      updateTimer();
+    }
+
+  }, 1000);
 };
 
 startButton.addEventListener("click", startTimer);
